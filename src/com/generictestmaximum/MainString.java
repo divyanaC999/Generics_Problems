50fbbcee984bbdcf87790f13eb27a0048a2fc3be
divyanaC999 <divyachourasiya999@gmail.com>
2022-03-08 00:25:45 +0530

    Added: Main

---
+++ src/com/generictestmaximum/MainString.java
@@ -0,0 +1,10 @@
+package com.generictestmaximum;
+
+public class MainString {
+
+	public static void main(String[] args) {
+		String str1 = "divya", str2 = "divy", str3 = "div";
+
+		System.out.println(MaxString.FindMaxString(str1, str2, str3));
+	}
+}
