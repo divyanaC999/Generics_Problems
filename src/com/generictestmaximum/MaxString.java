2dd281b1119936c89ffadcd273929f763574adf8
divyanaC999 <divyachourasiya999@gmail.com>
2022-03-08 00:24:17 +0530

    Added: Maximum String

---
+++ src/com/generictestmaximum/MaxString.java
@@ -0,0 +1,16 @@
+package com.generictestmaximum;
+
+public class MaxString {
+
+	public static String FindMaxString(String str1, String str2, String str3) {
+		if (str1.compareTo(str2) > 0 && str1.compareTo(str3) > 0) {
+			return str1;
+		} else if (str3.compareTo(str1) > 0 && str3.compareTo(str2) > 0) {
+			return str3;
+		} else {
+			return str2;
+		}
+	}
+}
+	
+
