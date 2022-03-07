17e3991b54294f55c4615bc70d5253689e96f7b5
divyanaC999 <divyachourasiya999@gmail.com>
2022-03-08 00:14:14 +0530

    intial commit

---
+++ src/com/generictestmaximum/IntergerMain.java
@@ -0,0 +1,12 @@
+package com.generictestmaximum;
+
+public class IntergerMain {
+
+	public static void main(String[] args) {
+
+		Integer firstNum = 1, secondNum = 9, thirdNum = 3;
+
+		System.out.println(MaxInteger.FindMaxNumber(firstNum, secondNum, thirdNum));
+	}
+
+}
